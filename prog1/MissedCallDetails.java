61b82b25bd284e89e03484a2d22b5f1d3d6ea504
PBopana <94296539+PBopana@users.noreply.github.com>
2023-05-26 20:37:46 +0530

    Create MissedCallDetails.java

---
+++ prog1/MissedCallDetails.java
@@ -0,0 +1,12 @@
+package prog1;
+import java.util.Calendar;
+
+final class MissedCallDetails { 
+	Calendar cal;
+	long phno;
+	public MissedCallDetails(Calendar cal,long phno)
+	{
+		this.cal=cal;
+		this.phno=phno;
+	}
+}
